8f03ca327c0b42e7a480424e52ae22e41e07ce5d
Lamin Sanneh <lamin.evra@gmail.com>
2024-03-11 21:55:29 +0000

    Loop over fixture object to test for each image in homepage response

--- cypress/e2e/image-page/list-image-with-tags.cy.js
+++ cypress/e2e/image-page/list-image-with-tags.cy.js
@@ -19,45 +19,82 @@ context('Single Photo Page After Clicking one on homepage', () => {
         cy.visit('/');
     });
 
+    // it('should navigate to correct(first) single photo page and have correct number of tags in dom', () => {
+    //     cy.get('.photo-link').eq(0).click();
+
+    //     cy.wait('@getPhoto1').its('response.body.tags')
+    //         .should('have.length', 3);
+
+    //     cy.location().should((loc) => {
+    //         expect(loc.href).to.contain(`${loc.host}/photos/${requiredApiPhotos[0].id}`)
+    //     })
+
+    //     cy.get('.tags-and-image-container img')
+    //         .eq(0).should('be.visible')
+    //         .should('have.length', 1)
+    //         .invoke('width')
+    //         .should('be.gt', 10);
+
+    //     cy.get('.tags-and-image-container .tag-component-container').filter(':not(:visible)').should('have.length', 1);
+    //     cy.get('.tags-and-image-container .tag-component-container').filter(':visible').should('have.length', requiredApiPhotos[0].tags.length);
+
+    //     requiredApiPhotos[0].tags.forEach((tag, index) => {
+    //         cy.get('.tags-and-image-container .tag-component-container').eq(index)
+    //             .should('have.attr', 'style', `left: ${tag.x_offset}%; top: ${tag.y_offset}%;`);
+    //     })
+    // })
+
+    // it('should navigate to correct(second) single photo page and have correct number of tags in dom', () => {
+    //     cy.get('.photo-link').eq(1).click();
+
+    //     cy.wait('@getPhoto2').its('response.body.tags')
+    //         .should('have.length', 2);
+
+    //     cy.location().should((loc) => {
+    //         expect(loc.href).to.contain(`${loc.host}/photos/${requiredApiPhotos[1].id}`)
+    //     })
+
+    //     cy.get('.tags-and-image-container img')
+    //         .eq(0).should('be.visible')
+    //         .should('have.length', 1)
+    //         .invoke('width')
+    //         .should('be.gt', 10);
+
+    //     cy.get('.tags-and-image-container .tag-component-container').filter(':not(:visible)').should('have.length', 1);
+    //     cy.get('.tags-and-image-container .tag-component-container').filter(':visible').should('have.length', requiredApiPhotos[1].tags.length);
+
+    //     requiredApiPhotos[1].tags.forEach((tag, index) => {
+    //         cy.get('.tags-and-image-container .tag-component-container').eq(index)
+    //             .should('have.attr', 'style', `left: ${tag.x_offset}%; top: ${tag.y_offset}%;`);
+    //     })
+    // })
 
 
-    it('should navigate to correct(first) single photo page and have correct number of tags in dom', () => {
-        cy.get('.photo-link').eq(0).click();
+    requiredApiPhotos.forEach((photo, index) => {
+        it(`should navigate to correct(indexed: ${index}) single photo page and have correct number of tags in dom`, () => {
+            cy.get('.photo-link').eq(index).click();
 
-        cy.wait('@getPhoto1').its('response.body.tags')
-            .should('have.length', 3);
+            cy.wait(`@getPhoto${index+1}`).its('response.body.tags')
+                .should('have.length', photo.tags.length);
 
-        cy.location().should((loc) => {
-            expect(loc.href).to.contain(`${loc.host}/photos/${requiredApiPhotos[0].id}`)
+            cy.location().should((loc) => {
+                expect(loc.href).to.contain(`${loc.host}/photos/${photo.id}`)
+            })
+
+            cy.get('.tags-and-image-container img')
+                .eq(0).should('be.visible')
+                .should('have.length', 1)
+                .invoke('width')
+                .should('be.gt', 10);
+
+            cy.get('.tags-and-image-container .tag-component-container').filter(':not(:visible)').should('have.length', 1);
+            cy.get('.tags-and-image-container .tag-component-container').filter(':visible').should('have.length', photo.tags.length);
+
+            photo.tags.forEach((tag, index) => {
+                cy.get('.tags-and-image-container .tag-component-container').eq(index)
+                    .should('have.attr', 'style', `left: ${tag.x_offset}%; top: ${tag.y_offset}%;`);
+            })
         })
 
-        cy.get('.tags-and-image-container img')
-            .eq(0).should('be.visible')
-            .should('have.length', 1)
-            .invoke('width')
-            .should('be.gt', 10);
-
-        cy.get('.tags-and-image-container .tag-component-container').filter(':not(:visible)').should('have.length', 1);
-        cy.get('.tags-and-image-container .tag-component-container').filter(':visible').should('have.length', requiredApiPhotos[0].tags.length);
-    })
-
-    it('should navigate to correct(second) single photo page and have correct number of tags in dom', () => {
-        cy.get('.photo-link').eq(1).click();
-
-        cy.wait('@getPhoto2').its('response.body.tags')
-            .should('have.length', 2);
-
-        cy.location().should((loc) => {
-            expect(loc.href).to.contain(`${loc.host}/photos/${requiredApiPhotos[1].id}`)
-        })
-
-        cy.get('.tags-and-image-container img')
-            .eq(0).should('be.visible')
-            .should('have.length', 1)
-            .invoke('width')
-            .should('be.gt', 10);
-
-        cy.get('.tags-and-image-container .tag-component-container').filter(':not(:visible)').should('have.length', 1);
-        cy.get('.tags-and-image-container .tag-component-container').filter(':visible').should('have.length', requiredApiPhotos[1].tags.length);
     })
 })
